614f035c117f3874da9fb2df51fb1d38d5b314a8
me <me@me-PC>
2022-12-27 15:24:43 +0530

    build 01

---
+++ TestNG1/src/assignment/maxmin.java
@@ -0,0 +1,16 @@
+package assignment;
+
+import org.testng.annotations.Test;
+
+public class maxmin extends gerenic
+{
+	@Test
+	public void test()
+	{
+		driver.get("https://www.flipkart.com");
+		
+	}
+
+
+
+}
